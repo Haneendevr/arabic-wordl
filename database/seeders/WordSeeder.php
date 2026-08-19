357224168f7165eb4b80eab793f03eeb0b01c373
Haneendevr <haneenabdulazizb@gmail.com>
2026-08-20 02:48:47 +0300

    Fix class name in WordSeeder

--- database/seeders/WordSeeder.php
+++ database/seeders/WordSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Word;
 
-class DatabaseSeeder extends Seeder
+class WordSeeder extends Seeder
 {
     public function run(): void
     {
